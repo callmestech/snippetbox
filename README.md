2b94c083324ec8c06a0f38cbb38513be536cba2b
Sergei Kharchikov <callmestech@gmail.com>
2023-03-20 23:00:59 +0000

    3.5 Isolating the ApplicationRoutes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Let's go!
 
-- [ ] **2. Foundations**
+- [x] **2. Foundations**
    - [x] 2.1 Installing GO
    - [x] 2.2 Project Setup and Enabling Modules
    - [x] 2.3 Web Application Basics
@@ -11,8 +11,11 @@
    - [x] 2.8 HTML Templating and Inheritance
    - [x] 2.9 Serving Static Files
    - [x] 2.10 The http.Handler Interface 
-- [ ] **3. Configuration and Error Handling**
+- [x] **3. Configuration and Error Handling**
    - [x] 3.1 Managing configuration settings
    - [x] 3.2 Leveled Logging 
    - [x] 3.3 Dependency Injection
    - [x] 3.4 Centralized Error Handling
+   - [x] 3.5 Isolating the Application Routes
+- [ ] **4. Database-Driven Responses** 
+   - [ ] 4.1 Setting Up MySQL
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -29,26 +29,12 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves files out of the "./ui/static" directory
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
+
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
